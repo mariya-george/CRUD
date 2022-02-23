3695bbf86f068ae47cfc3f0e349216261aa3fe8f
mariya-george <100010483+mariya-george@users.noreply.github.com>
2022-02-23 12:45:25 +0530

    Delete Employee_registration directory

--- Employee_registration/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class EmployeeRegistrationConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Employee_registration'
--- Employee_registration/forms.py
+++
@@ -1,8 +0,0 @@
-from .models import Employee
-from django.forms import ModelForm
-
-class EmployeeForm(ModelForm):
-    class Meta:
-        model=Employee
-        fields='FullName','mobile','Emp_code','position'
-        labels={'FullName':'Name','Emp_code':'Employee ID','mobile':'Phone Number'}
